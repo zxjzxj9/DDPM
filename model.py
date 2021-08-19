df46fcc865f93e8e47312b37331606626d257655
Victor Zhang <zxjzxj9@gmail.com>
2021-08-19 21:55:41 +0800

    Add example code for Diffusion Layer

---
+++ model.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Diffusion(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def _forward(self, x):
+        pass
+
+    def _backward(self, x):
+        pass
+
+    def forward(self, x):
+        pass
