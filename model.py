a74bca6f22ccf79c4c4d2e76b391d2f6173411bd
Victor Zhang <zxjzxj9@gmail.com>
2021-08-20 22:33:16 +0800

    Add alpha value into the module

--- model.py
+++ model.py
@@ -1,13 +1,19 @@
 #! /usr/bin/env python
 
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 class Diffusion(nn.Module):
 
-    def __init__(self):
+    def __init__(self, alpha):
         super().__init__()
+
+        if alpha > 1 or alpha < 0:
+            raise Exception(f"Invalid alpha value: {alpha:12.6f}")
+        self.beta1 = math.sqrt(alpha)
+        self.beta2 = math.sqrt(1.0-alpha)
 
     def _forward(self, x):
         pass
