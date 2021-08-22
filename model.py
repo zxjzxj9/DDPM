fa9270e6001aeb1ccc42baaecde54fc9c0b0bf62
Victor Zhang <zxjzxj9@gmail.com>
2021-08-22 22:06:14 +0800

    Add ResNetBlock module

--- model.py
+++ model.py
@@ -5,6 +5,14 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class ResNetBlock(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
 class PixelCNN(nn.Module):
     pass
 
