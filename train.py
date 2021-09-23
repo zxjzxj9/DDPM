d71217f1d97a364fa618fe65a41909768c0ca44c
Victor Zhang <zxjzxj9@gmail.com>
2021-09-23 23:04:59 +0800

    Add argument parser

--- train.py
+++ train.py
@@ -1,5 +1,12 @@
 #! /usr/bin/env python
 
+import yaml
+import argparse
+
+arg = argparse.ArgumentParser("DDPM Parameters")
+arg.add_argument("-c", "--config", type=str, default="config.yml", help="Param config files")
 
 if __name__ == "__main__":
-    pass
+    opt = arg.parse_args()
+    with open(opt.config, "r") as fin:
+        conf = yaml.SafeLoader(fin)
